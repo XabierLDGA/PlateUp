9a73e3c056c9e75a3759bace3aea9d8ae28da961
Xabi <xabloparm@gmail.com>
2026-04-08 13:31:43 +0200

    Cambiado el localhost para no dar fallo al poner el capacitor y levantar un emulador

--- Vue/plateup-frontend/src/services/api.js
+++ Vue/plateup-frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const TOKEN_KEY = 'plateup_auth_token'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
